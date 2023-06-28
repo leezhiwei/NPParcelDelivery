52e759b4b6bcb44a7549049c03a27ecab3e09685
Lim-Jiaxian <limjiaxian2@gmail.com>
2023-06-28 17:52:12 +0800

    Updated DAL for Delivery Insert

--- NPParcelDeliveryServiceAssignment/Controllers/DeliveryController.cs
+++ NPParcelDeliveryServiceAssignment/Controllers/DeliveryController.cs
@@ -64,7 +64,30 @@ namespace NPParcelDeliveryServiceAssignment.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Insert(IFormCollection collection)
         {
+            Parcel p = new Parcel {
+                ItemDescription = collection["ItemDescription"],
+                SenderName = collection["SenderName"],
+                SenderTelNo = collection["SenderTelNo"],
+                ReceiverName = collection["ReceiverName"],
+                ReceiverTelNo = collection["ReceiverTelNo"],
+                DeliveryAddress = collection["DeliveryAddress"],
+                FromCity = collection["FromCity"],
+                FromCountry = collection["FromCountry"],
+                ToCity = collection["ToCity"],
+                ToCountry = collection["ToCountry"],
+                ParcelWeight = Convert.ToDouble(collection["ParcelWeight"]),
+                DeliveryCharge = Convert.ToDecimal(collection["DeliveryCharge"]),
+                Currency = collection["Currency"],
+                TargetDeliveryDate = Convert.ToDateTime(collection["TargetDeliveryDate"]),
+                DeliveryStatus = collection["DeliveryStatus"],
+                DeliveryManID = Convert.ToInt32(collection["DeliveryManID"]),
+            };
+
+            pdal.Add(p);
+
             return RedirectToAction("Insert");
+
+
         }
 
         // GET: DeliveryController/Edit/5
