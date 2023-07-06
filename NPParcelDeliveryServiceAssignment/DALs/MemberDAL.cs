171599b32d8688d5c329c85840740e702e1f5846
Peter <petr.liauos@gmail.com>
2023-07-06 17:42:25 +0800

    Feature 1 - Register completed

--- NPParcelDeliveryServiceAssignment/DALs/MemberDAL.cs
+++ NPParcelDeliveryServiceAssignment/DALs/MemberDAL.cs
@@ -47,10 +47,40 @@ namespace NPParcelDeliveryServiceAssignment.DALs
             return memberList;
         }
 
-        public Member Addmember(string email, string password)
+        public int AddMember(Member member)
         {
-            return null;
-        }
+            // Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify an INSERT SQL statement which will
+            //return the auto-generated StaffID after insertion
+            cmd.CommandText = @"INSERT INTO Member (Name, Salutation, TelNo, EmailAddr, Password, BirthDate, City, Country)
+                                OUTPUT INSERTED.MemberID
+                                VALUES(@name, @sal, @tel, @ema, @pa, @bd, @ci, @co)";
+            //Define the parameters used in SQL statement, value for each parameter
+            //is retrieved from respective class's property.
+            cmd.Parameters.AddWithValue("@name", member.Name);
+            cmd.Parameters.AddWithValue("@sal", member.Salutation);
+			cmd.Parameters.AddWithValue("@tel", member.TelNo);
+			cmd.Parameters.AddWithValue("@ema", member.EmailAddr);
+			cmd.Parameters.AddWithValue("@pa", member.Password);
+			cmd.Parameters.AddWithValue("@bd", member.BirthDate);
+			cmd.Parameters.AddWithValue("@ci", member.City);
+			cmd.Parameters.AddWithValue("@co", member.Country);
+			//A connection to database must be opened before any operations made.
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+			conn.Open();
+            //ExecuteScalar is used to retrieve the auto-generated
+            //StaffID after executing the INSERT SQL statement
+            member.MemberID = (int)cmd.ExecuteScalar();
+            //A connection should be closed after operations.
+            conn.Close();
+            //Return id when no error occurs.
+            return member.MemberID;
+		 }
+        
         public Member CheckMember(string email, string password)
         {
             List<Member> mlist = GetAllMember();
