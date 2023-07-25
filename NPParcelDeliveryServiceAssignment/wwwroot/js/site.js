fb451b232af4a4f966ae888d5ca8766857463bfa
Lee Zhi Wei <s10239812@connect.np.edu.sg>
2023-07-25 17:34:50 +0800

    Front Office Conversion

--- NPParcelDeliveryServiceAssignment/wwwroot/js/site.js
+++ NPParcelDeliveryServiceAssignment/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
